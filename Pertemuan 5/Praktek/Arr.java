7f9027db522ab3fd2edaaf3f9b27b792144b1667
Rama <80207084+Rama123-Rr@users.noreply.github.com>
2021-04-06 03:49:59 +0700

    Delete Pertemuan 5/Praktek directory

--- Pertemuan 5/Praktek/Arr.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-class Arr{
-    ArrayList<Integer> sa = new ArrayList<Integer>();
-    sa.add("2");
-
-}
--- Pertemuan 5/Praktek/Uji.java
+++
@@ -1,8 +0,0 @@
-public class Uji {
-    public static void main(String[] args) {
-        Arr coba = new Arr();
-
-        coba.pertambahan(2,3);
-        System.out.println("L1" + L1 + "+" + L2 + "L2 =" +pertambahan(L1,L2));
-    }
-}
